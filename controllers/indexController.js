b4b5ea63a27a2a030ecd5bdae85a7ac45a8f2135
Shadab Mahtabi <shadabmanpuri@gmail.com>
2024-04-20 14:31:40 +0530

    again

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -9,7 +9,6 @@ const { sendToken } = require("../utils/sendToken");
 exports.homepage = catchAsynchErrors(async (req, res, next) => {
   res.json({
     message: "Secure homepage",
-    // user: req.user
   });
 });
 
@@ -17,36 +16,45 @@ exports.homepage = catchAsynchErrors(async (req, res, next) => {
   Controller function to handle registration of a user.
 */
 exports.userSignUp = catchAsynchErrors(async (req, res, next) => {
-  let user = await new userModel(req.body).save();
-
   // Sending back the created user with a response code of 201 which means 'Created'
-
-  sendToken(user, 201, res, next);
+  sendToken(await new userModel(req.body).save(), 201, res, next);
 });
 
 /*
   Controller function to handle login of a user.
 */
+
 exports.userSignIn = catchAsynchErrors(async (req, res, next) => {
-  let user = await userModel
+  const user = await userModel
     .findOne({ email: req.body.email })
-    .select("+password")
-    .exec();
-
-  if (!user)
-    return next(new ErrorHandler("No user exists with this credentials", 404));
-
-  let isMatch = user.comparePassword(req.body.password);
-
-  if (!isMatch) return next(new ErrorHandler("Wrong Credentials", 400));
-
-  sendToken(user, 200, res, next);
+    .select("+password");
+  return !user || !user.comparePassword(req.body.password)
+    ? next(new ErrorHandler("Wrong Credentials", 400))
+    : sendToken(user, 200, res, next);
 });
+
+// exports.userSignIn = catchAsynchErrors(async (req, res, next) => {
+//   let user = await userModel
+//     .findOne({ email: req.body.email })
+//     .select("+password")
+//     .exec();
+
+//   if (!user)
+//     return next(new ErrorHandler("No user exists with this credentials", 404));
+
+//   let isMatch = user.comparePassword(req.body.password);
+
+//   if (!isMatch) return next(new ErrorHandler("Wrong Credentials", 400));
+
+//   sendToken(user, 200, res, next);
+// });
 
 /*
   Controller function to handle login of a user.
 */
 exports.userSignOut = catchAsynchErrors(async (req, res, next) => {
-  res.clearCookie("token");
-  res.status(200).json({ message: "Successfully Signed Out!" });
+  res
+    .clearCookie("token")
+    .status(200)
+    .json({ message: "Successfully Signed Out!" });
 });
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,15 +4,31 @@ const { catchAsynchErrors } = require("./catchAsynchErrors");
 const userModel = require("../models/userModel");
 
 exports.isAuthenticated = catchAsynchErrors(async (req, res, next) => {
-  let { token } = req.cookies;
+  // let { token } = req.cookies;
 
-  if (!token) {
-    return next(new ErrorHandler("Please signin to access the resource.", 401));
-  }
+  // if (!token) {
+  //   return next(new ErrorHandler("Please signin to access the resource.", 401));
+  // }
 
-  let { id } = jwt.verify(token, process.env.JWT_SECRET);
+  // let { id } = jwt.verify(token, process.env.JWT_SECRET);
 
-  //   res.json({ id, token });
+  // //   res.json({ id, token });
   //   req.user = await userModel.findById(id).exec();
-  next();
+  // next();
+
+  // req.cookies.token
+  //   ? jwt.verify(req.cookies.token, process.env.JWT_SECRET)
+  //   : next(new ErrorHandler("Please signin to access the resource.", 401));
+  // next();
+
+  const { token } = req.cookies;
+  token && jwt.verify(token, process.env.JWT_SECRET);
+  req.user = token
+    ? await userModel.findById(jwt.decode(token).id).exec()
+    : null;
+  next(
+    token
+      ? null
+      : new ErrorHandler("Please signin to access the resource.", 401)
+  );
 });
