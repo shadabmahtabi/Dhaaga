ed9e27e08910411ceaad1207b2ac77e52ae8d0c1
Shadab Mahtabi <115717013+shadabmahtabi@users.noreply.github.com>
2024-04-21 11:49:13 +0530

    Update2 app.js

--- app.js
+++ app.js
@@ -14,13 +14,7 @@ require('./models/database').connectDatabase();
 const logger = require('morgan');
 app.use(logger('dev'));
 
-// BodyParser - Parsing the request body
-/*
-    This middleware is responsible for parsing incoming requests 
-    and populating req.body with any data sent in a JSON payload.  
-    If the Content-Type of the request is application/x-www-form-urlencoded,  
-    then the middleware will parse the content of this request to make it available as params on the req.body object.
-*/
+// BodyParser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -45,20 +39,11 @@ app.use('/', indexRoutes)
 // Error Handling
 const ErrorHandler = require('./utils/ErrorHandler');
 const { generatedErrors } = require('./middlewares/error');
-/*
-404 - Page not found error
-The server could not find the requested resource. 
-*/
 app.all( '*', (req, res, next) => {
     next(new ErrorHandler("Requested Url Not Found", 404))
 });
   
 // Adding middleware to handle any errors that occur in our application
-/*
-    Any unhandled errors will be caught here and the following data will be sent:
-        .message: A short description of the error.
-        .status: The HTTP status code to send with the response.
-*/
 app.use(generatedErrors);
 
 
