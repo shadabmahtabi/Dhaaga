ff6c94839384b463560a71518f4425e52adb7fef
Shadab Mahtabi <115717013+shadabmahtabi@users.noreply.github.com>
2024-04-21 11:33:18 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,23 +4,6 @@ const { catchAsynchErrors } = require("./catchAsynchErrors");
 const userModel = require("../models/userModel");
 
 exports.isAuthenticated = catchAsynchErrors(async (req, res, next) => {
-  // let { token } = req.cookies;
-
-  // if (!token) {
-  //   return next(new ErrorHandler("Please signin to access the resource.", 401));
-  // }
-
-  // let { id } = jwt.verify(token, process.env.JWT_SECRET);
-
-  // //   res.json({ id, token });
-  //   req.user = await userModel.findById(id).exec();
-  // next();
-
-  // req.cookies.token
-  //   ? jwt.verify(req.cookies.token, process.env.JWT_SECRET)
-  //   : next(new ErrorHandler("Please signin to access the resource.", 401));
-  // next();
-
   const { token } = req.cookies;
   token && jwt.verify(token, process.env.JWT_SECRET);
   req.user = token
